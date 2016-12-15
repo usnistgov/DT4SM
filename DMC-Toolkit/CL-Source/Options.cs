0679477d508ff0050e3b524f087b53cf167b2d20
krimas <krimas@nist.gov>
2016-12-15 10:02:51 -0500

    CLI 0.0.0.1

---
+++ DMC-Toolkit/CL-Source/Options.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace NIST.DMCLI
+{
+    public abstract class FileOption
+    {
+        [Option('f', "file", Required = true, HelpText = "Absolute path to the file to process.")]
+        public string File { get; set; }
+
+    }
+
+    public class SignOption: FileOption
+    {
+
+        [Option('c', "cert", Required = true, HelpText = "Certificate friendly name.")]
+        public string Certificate { get; set; }
+
+        [Option("path", HelpText ="Path to the certificate.")]
+        public string Path { get; set; }
+
+        [Option("pwd", HelpText ="Password to access the certificate.")]
+        public string Password { get; set; }
+    }
+
+    public class VerifyOption: FileOption 
+    {
+
+    }
+
+    public class VerifyWithMetadata: FileOption
+    {
+
+    }
+    public class SignWithMetadata: SignOption
+    {
+        [OptionArray("metadata", Required = true, HelpText ="Metadata to embed in the signature.")]
+        public String[] Metadata { get; set; }
+    }
+
+    public class Options
+    {
+        [VerbOption("sign", HelpText = "Sign a file.")]
+        public SignOption SignVerb { get; set; }
+
+        [VerbOption("verify",HelpText ="Verify the signature(s) in a file.")]
+        public VerifyOption VerifyVerb { get; set; }
+
+        [VerbOption("signm", HelpText ="Sign a file and add metadata.")]
+        public SignWithMetadata SignMVerb { get; set; }
+
+        [VerbOption("verifym", HelpText = "Verify the signature(s) in a file that contains metadata.")]
+        public VerifyWithMetadata VerifyMVerb { get; set; }
+    }
+}
---
+++ DMC-Toolkit/CL-Source/Program.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using NIST.DMC;
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NIST.DMCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //TODO: remove Console.read()
+            X509Certificate2 x2 = null;
+            FormatManager fm = null;
+            
+            string invokedVerb = null;
+            object invokedVerbInstance = null;
+
+
+            var options = new Options();
+            if (!CommandLine.Parser.Default.ParseArguments(args, options,
+              (verb, subOptions) =>
+              {
+
+                  invokedVerb = verb;
+                  invokedVerbInstance = subOptions;
+              }))
+            {
+
+                WriteRed("Incorrect arguments.");
+                Console.Read();
+                Environment.Exit(CommandLine.Parser.DefaultExitCodeFail);
+            }
+
+            #region simple signature
+            if (invokedVerb == "sign")
+            {
+                
+                var signoptioninstance = (SignOption)invokedVerbInstance;
+
+                if(!File.Exists(signoptioninstance.File))
+                    Environment.Exit(1);
+
+                if (signoptioninstance.Path != null && signoptioninstance.Password != null)
+                    CertificatesManager.AddProtectedCertificate(signoptioninstance.Path, signoptioninstance.Password);
+
+
+                x2 = CertificatesManager.FindByFriendlyName(signoptioninstance.Certificate);
+                if (x2 != null)
+                {
+                    fm = FormatManagerFactory.GetInstance(signoptioninstance.File);
+                    fm.EncodeAndSign(x2, signoptioninstance.File);
+                    WriteGreen("Signed " + signoptioninstance.File);
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    WriteRed("Could not find certificate.");
+                    Environment.Exit(1);
+                }
+
+                Console.Read();
+            }
+            #endregion
+
+            #region simple signature verification
+            if (invokedVerb == "verify")
+            {
+                var verifyoptioninstance = (VerifyOption)invokedVerbInstance;
+                if (!FileExists(verifyoptioninstance.File))
+                    Environment.Exit(1);
+
+                fm = FormatManagerFactory.GetInstance(verifyoptioninstance.File);
+                List<KeyValuePair<X509Certificate2, bool>> UsedCertificates = new List<KeyValuePair<X509Certificate2, bool>>();
+                if(fm.VerifyFile(verifyoptioninstance.File, ref UsedCertificates))
+                {
+                    WriteGreen("All signatures in " + verifyoptioninstance.File + " were verified.");
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    WriteRed("Could not verify all signatures in " + verifyoptioninstance.File);
+                    Environment.Exit(1);
+                }
+
+
+                Console.WriteLine("Verified {0}",verifyoptioninstance.File);
+                Console.Read();
+            }
+            #endregion
+
+            #region signature with metadata
+            if(invokedVerb == "signm")
+            {
+                var signminstance = (SignWithMetadata)invokedVerbInstance;
+
+                if (!FileExists(signminstance.File))
+                    Environment.Exit(1);
+
+                //make sure all the metadata fields are properly formatted
+                signminstance.Metadata.ToList().ForEach(field =>
+                {
+                    if (!IsValidMetadata(field))
+                    {
+                        WriteRed("Invalid field " + field);
+                        Environment.Exit(1);
+                    }
+                    else
+                        WriteGreen("Valid field " + field);
+                });
+
+                STEPTRACE4PLOT fileManager = new STEPTRACE4PLOT(signminstance.File);
+                STEPInterface.Elements elts = fileManager.GetBlocks();
+                var b = elts.CreateBlock();
+                signminstance.Metadata.ToList().ForEach(field =>
+                {
+                    string[] data = field.Split(':');
+                    elts.AddMetadata(b, data[0], data[1]);
+                });
+
+                x2 = CertificatesManager.FindByFriendlyName(signminstance.Certificate);
+                if (x2 != null)
+                {
+                    fileManager.EncodeAndSign(x2, signminstance.File,b);
+                    WriteGreen("Signed " + signminstance.File);
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    WriteRed("Could not find certificate.");
+                    Environment.Exit(1);
+                }
+                Console.WriteLine(elts.FullText());
+                Console.Read();
+            }
+            #endregion
+
+            #region  signature with metadata verification
+            if (invokedVerb == "verifym")
+            {
+                var verifym = (VerifyWithMetadata)invokedVerbInstance;
+                if (!FileExists(verifym.File))
+                    Environment.Exit(1);
+
+                fm = new STEPTRACE4PLOT(verifym.File);
+                List<KeyValuePair<X509Certificate2, bool>> UsedCertificates = new List<KeyValuePair<X509Certificate2, bool>>();
+                if (fm.VerifyFile(verifym.File, ref UsedCertificates))
+                {
+                    WriteGreen("All signatures in " + verifym.File + " were verified.");
+                    //Environment.Exit(0);
+                }
+                else
+                {
+                    WriteRed("Could not verify all signatures in " + verifym.File);
+                    //Environment.Exit(1);
+                }
+
+
+                Console.WriteLine("Verified {0}", verifym.File);
+                Console.Read();
+            }
+            #endregion
+        }
+
+        private static void SwitchRed()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+        }
+
+        private static void SwitchGreen()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+        }
+
+        private static void WriteGreen(string text)
+        {
+            SwitchGreen();
+            Console.WriteLine(text);
+        }
+
+        private static void WriteRed(string text)
+        {
+            SwitchRed();
+            Console.WriteLine(text);
+        }
+
+        private static bool FileExists(string FilePath)
+        {
+            bool exists = true;
+            if (!File.Exists(FilePath))
+            {
+                WriteRed("Could not find " + FilePath);
+                exists = false;
+            }
+            return exists;
+        }
+
+        private static bool IsValidMetadata(string field)
+        {
+
+            bool valid = false;
+            if (field.IndexOf(':') > 1 && field.IndexOf(':') < field.Length - 1)
+                valid = true;
+            return valid;
+        }
+    }
+}
